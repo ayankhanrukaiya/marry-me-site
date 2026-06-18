32633c87882672335a303019ec1fd4b2881d204b
Rukaiya 💗 <mosesvvdemma7148@gmail.com>
2026-06-18 12:38:31 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -1,25 +1,24 @@
 const noBtn = document.getElementById("noBtn");
 const yesBtn = document.getElementById("yesBtn");
 
-const proposalBox = document.getElementById("proposalBox");
-const resultBox = document.getElementById("resultBox");
+const proposalPage = document.getElementById("proposalPage");
+const resultPage = document.getElementById("resultPage");
 
 function moveNoButton() {
-    const maxX = window.innerWidth - 150;
-    const maxY = window.innerHeight - 100;
+    const maxX = 250;
+    const maxY = 100;
 
     const randomX = Math.floor(Math.random() * maxX);
     const randomY = Math.floor(Math.random() * maxY);
 
-    noBtn.style.position = "fixed";
     noBtn.style.left = randomX + "px";
     noBtn.style.top = randomY + "px";
 }
 
-noBtn.addEventListener("mouseover", moveNoButton);
+noBtn.addEventListener("mouseenter", moveNoButton);
 noBtn.addEventListener("click", moveNoButton);
 
 yesBtn.addEventListener("click", () => {
-    proposalBox.classList.add("hidden");
-    resultBox.classList.remove("hidden");
+    proposalPage.classList.add("hidden");
+    resultPage.classList.remove("hidden");
 });
